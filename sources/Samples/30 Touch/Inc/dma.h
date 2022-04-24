fa714b613ccccd9a50392ffdbac1833dbdf9d50d
Sasha7b9 <Sasha7b9@tut.by>
2022-04-24 21:23:31 +0300

    *

--- sources/Samples/30 Touch/Inc/dma.h
+++ sources/Samples/30 Touch/Inc/dma.h
@@ -1,15 +1,5 @@
-#pragma once
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include "stm32f7xx_hal.h"
 
-    extern DMA_HandleTypeDef hdma_memtomem_dma2_stream0;
+extern DMA_HandleTypeDef hdma_memtomem_dma2_stream0;
 
-    void MX_DMA_Init(void);
-
-#ifdef __cplusplus
-}
-#endif
+void MX_DMA_Init(void);
