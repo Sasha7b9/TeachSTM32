22e8b731c0a0cd3aff4045ae788140d9fd286da9
Sasha7b9 <Sasha7b9@tut.by>
2022-04-24 22:40:19 +0300

    *

--- sources/Test/Src/main.cpp
+++ sources/Test/Src/main.cpp
@@ -75,7 +75,6 @@ int main(void)
 	}
 }
 
-
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	if (GPIO_Pin == GPIO_PIN_7)
@@ -83,24 +82,3 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 		TS_flag = 1;
 	}
 }
-
-
-#ifdef USE_FULL_ASSERT
-
-/**
-   * @brief Reports the name of the source file and the source line number
-   * where the assert_param error has occurred.
-   * @param file: pointer to the source file name
-   * @param line: assert_param error line source number
-   * @retval None
-   */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-	/* USER CODE BEGIN 6 */
-	/* User can add his own implementation to report the file name and line number,
-	  ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	  /* USER CODE END 6 */
-
-}
-
-#endif
